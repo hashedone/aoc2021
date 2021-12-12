084df7c3cccbc1490fe91261da149993177de424
Bartłomiej Kuras <hash.deb@gmail.com>
2021-12-12 18:40:58 +0100

    Haskell day1 solution

---
+++ hask/app/Day1.hs
@@ -0,0 +1,19 @@
+module Day1 where
+
+import System.IO
+import Control.Arrow
+
+zipSkipping :: Int -> [Int] -> ([Int], [Int])
+zipSkipping = (&&&) id . drop
+
+(.:) = (.) . (.)
+
+solve :: Int -> [Int] -> Int
+solve = (length . filter id . uncurry (zipWith (<))) .: zipSkipping
+
+part1 = solve 1
+part2 = solve 3
+
+main :: IO ()
+main = (print . (part1 &&& part2) . map read . lines) =<< getContents
+
